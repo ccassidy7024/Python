f415fbe5a2d026345e4efce3834f058ae369038a
Caitlin Cassidy <ccassidy7024@students.mchenry.edu>
2025-03-02 21:01:22 -0600

    Added Data Types Python Program

---
+++ datatypes.py
@@ -0,0 +1,20 @@
+# This is a Python program to demonstrate different data types
+
+# Literal
+print("Literal:", 23)
+
+# Integer
+y = -44
+print("Integer:", y)
+
+# Float
+z = 72.3
+print("Float:", z)
+
+# String
+name = "Hendrix"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
