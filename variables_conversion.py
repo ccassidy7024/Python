09d14ad369db42bb451e3292447b37429f5cba65
Caitlin Cassidy <ccassidy7024@students.mchenry.edu>
2025-03-02 21:44:01 -0600

    Added variable kg to lb conversion Python program

---
+++ variables_conversion.py
@@ -0,0 +1,5 @@
+#Python Program that converts kilograms to pounds 
+
+_userWeight = input("Weight kilograms: ")
+_userLbs = int(_userWeight) * 2.2
+print(str(_userWeight) + " kilograms equals " + str(_userLbs) + " pounds")
