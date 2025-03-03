0ae9831f31bf33895a4bb16df18043f2a23716d8
Caitlin Cassidy <ccassidy7024@students.mchenry.edu>
2025-03-02 21:28:06 -0600

    Added Math Operations Python Program

---
+++ mathoperations.py
@@ -0,0 +1,8 @@
+#Python Program to calculate the area of a rectangle
+
+length = 20
+width = 45
+area = length * width
+
+
+print("The area of the rectangle with a length of", length, "and a width of", width, "is", area)
