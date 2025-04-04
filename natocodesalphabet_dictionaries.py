64c4b6023ced7e8936e7bcb19c8f539861200892
Caitlin Cassidy <ccassidy7024@students.mchenry.edu>
2025-04-03 22:32:55 -0500

    Updated Python program using directories to convert letters to NATO alphabet

--- natocodesalphabet_dictionaries.py
+++ natocodesalphabet_dictionaries.py
@@ -1,10 +1,8 @@
 #Python program that uses a created dictionary  to translate letters to NATO Phonetic Alphabet terms 
 # and returns word input by user in NATO codes
 
-#Program logic organized in main function
-def main():
-    #Dictionary with key:value pairs of letters and NATO codes for conversion
-    nato_alphabet = {
+#Dictionary with key:value pairs of letters and NATO codes for conversion
+nato_alphabet = {
         "A": "Alpha",
         "B": "Bravo",
         "C": "Charlie",
@@ -32,8 +30,11 @@ def main():
         "Y": "Yankee",
         "Z": "Zulu"
     }
+#Program logic organized in main function
+def main():
     word = input("Enter a word to translate to the NATO Phonetic Alphabet: ").upper() #takes user input for word to covert spelling in NATO codes
     word_list = [nato_alphabet[letter] for letter in word] #searches dictionary to pair key of letter with NATO code value
     print(word_list) #prints the entered word by listing NATO code values
 #calls main function to run program
+
 main()
